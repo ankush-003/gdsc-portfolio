48b58336195314e2103ae7a2264231c489d93639
Ankush <anshhv2003@gmail.com>
2022-11-26 06:23:24 +0000

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -3,7 +3,7 @@ function Hero() {
         <div
             className='hero flex flex-col items-start text-left justify-center min-h-screen px-[25px] max-w-7xl mx-auto'
             id='hero'>
-            <h1>Hi, my name is</h1>
+            <h1 className='text-emerald font-mono'>Hi, my name is</h1>
             <h2>Rick Astley</h2>
             <h3>I&apos;m never gonna give you up</h3>
             <p>
